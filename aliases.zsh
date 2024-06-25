b7b8e98a99a2018c02fb906581c58c24fa538b65
Léonard Binet <leonard.binet@traace.co>
2024-06-25 12:54:36 +0200

    fix lokalize push translations

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,6 @@
 alias zshconfig="vi ~/.zshrc"
 alias lg=lazygit
+alias ld=lazydocker
 # python virtualenv activation
 alias activate=". env/bin/activate"
 alias act_pr="act pull_request"
@@ -8,4 +9,18 @@ alias refresh_test_db="npm run _test docker:down && npm run _test docker:up && n
 alias _dev="dotenv -e .dev.env"
 alias _test="dotenv -e .test.env"
 alias dev_front="dotenv -e .dev.env npm run start"
-alias push_localize="lokalise2 --project-id ${LOCALIZE_TRAACE_PROJECT} -t ${LOCALIZE_WRITE_TOKEN} file upload --file src/locales/en/translation.json --lang-iso en && lokalise2 --project-id ${LOCALIZE_TRAACE_PROJECT} -t ${LOCALIZE_WRITE_TOKEN} file upload --file src/locales/fr/translation.json --lang-iso fr && lokalise2 --project-id ${LOCALIZE_TRAACE_PROJECT} -t ${LOCALIZE_WRITE_TOKEN} file upload --file src/locales/de/translation.json --lang-iso de"
+
+
+push_localize_translation()
+{
+  LANG=$1
+  lokalise2 --project-id ${LOCALIZE_TRAACE_PROJECT} -t ${LOCALIZE_WRITE_TOKEN} file upload --file src/locales/${LANG}/translation.json --lang-iso ${LANG}
+}
+
+pull_localize_translation() {
+  lokalise2 --project-id ${LOCALIZE_TRAACE_PROJECT} -t ${LOCALIZE_WRITE_TOKEN} file download --original-filenames=false --bundle-structure src/locales/\%LANG_ISO\%/translation.%FORMAT% --json-unescaped-slashes --replace-breaks=false --format json --indentation 2sp --export-sort first_added --plural-format i18next_v4
+}
+
+alias pull_localize="pull_localize_translation"
+
+alias push_localize="push_localize_translation en && push_localize_translation fr && push_localize_translation de"
